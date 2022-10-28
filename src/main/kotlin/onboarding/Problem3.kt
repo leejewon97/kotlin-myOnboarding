df7eab39bee601c71c4b75732e20607d894e2f26
Jtility <leejewon97@gmail.com>
2022-10-29 04:57:24 +0900

    solve pr3

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,19 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+//    TODO("프로그램 구현")
+    var count = 0
+    var carNum = 1
+    var digit = 0
+    while (carNum <= number) {
+        var tmp = carNum
+        while (tmp > 0) {
+            digit = tmp % 10
+            if (digit == 6 || digit == 3 || digit == 9)
+                count++
+            tmp /= 10
+        }
+        carNum++
+    }
+    return count
 }
