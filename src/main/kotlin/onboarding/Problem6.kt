c94cb0fe1a696af0a5b7c3005ecf151f34c08daf
Jtility <leejewon97@gmail.com>
2022-10-30 06:37:12 +0900

    implement validCheck(+email +nickname) pr6

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,7 @@
 package onboarding
 
+import java.util.regex.Pattern
+
 fun solution6(forms: List<List<String>>): List<String> {
 //    TODO("프로그램 구현")
     if (false == validCheck(forms))
@@ -21,7 +23,30 @@ fun solution6(forms: List<List<String>>): List<String> {
 }
 
 fun validCheck(forms: List<List<String>>): Boolean {
-    return true
+	if (forms.size !in 1..10000)
+		return false
+	else if (!emailCheck(forms))
+		return false
+	else if (!nickCheck(forms))
+		return false
+    else
+		return true
+}
+
+fun nickCheck(forms: List<List<String>>): Boolean {
+	for (crew in forms) {
+		if (!Pattern.matches("^[ㄱ-ㅎㅏ-ㅣ가-힣]{1,19}$", crew[1]))
+			return false
+	}
+	return true
+}
+
+fun emailCheck(forms: List<List<String>>): Boolean {
+	for (crew in forms) {
+		if (!Pattern.matches("^[a-zA-Z0-9]{1,9}+@email.com$", crew[0]))
+			return false
+	}
+	return true
 }
 
 fun checkOverlap(crew: List<String>, i: Int, forms: List<List<String>>): List<String> {
