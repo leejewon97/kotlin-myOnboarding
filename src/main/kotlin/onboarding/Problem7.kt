1d6ee022dfbf9271a89dc0a3a40c68930ab3bc2f
Jtility <leejewon97@gmail.com>
2022-11-01 10:25:23 +0900

    refactoring 'mutua'l to 'friend', reverse sort in organizeResult, implement exclude my friends in recommend friends list

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -2,7 +2,7 @@ package onboarding
 
 // 1. friends 에서 user 찾기
 // 2. user의 친구목록 생성 (중복불가)
-// 3. 함아친 목록 생성 (ex : [a, j, a, j])
+// 3. 친구친구 목록 생성 (ex : [a, j, a, j])
 // 4. 3에서 중복하는 만큼 +10  (ex : [a, 20], [j, 20])
 //	 4-1. 새로 들어오면, 생성 후 10
 //	 4-2. 이미 존재하면, 해당 아이디 +10
@@ -15,32 +15,29 @@ fun solution7(
 ): List<String> {
 //    TODO("프로그램 구현")
 	val result: List<String>
-	val friendsList: List<String> = make4ndsList(user, friends)
-	val mutual4ndList: List<String> = makeMutual4ndList(friendsList, friends)
-	val mutualScore = calcScore(mutual4ndList, 10)
-	val visitScore = calcScore(visitors, 1).toMutableList()
-	val plusScore = mutableListOf<List<Any>>()
-	// [b,10] [j,20]
-	// [a,1] [b,1] [c,2] [j,1]
-	for (mut4nd in mutualScore) {
+	val userFriendsList: List<String> = makeUser4ndsList(user, friends)
+	val friend4ndList: List<String> = make4nd4ndList(userFriendsList, friends, user)
+	val fr4ndScore = calcScore(friend4ndList, 10, userFriendsList)
+	val visitScore = calcScore(visitors, 1, userFriendsList)
+	val plusScore = fr4ndScore
+	for (fr4nd in plusScore) {
 		var visit4nd = 0
 		while (visit4nd < visitScore.size) {
-			if (mut4nd[0] == visitScore[visit4nd][0]) {
-				plusScore.add(listOf(mut4nd[0], mut4nd[1] as Int + visitScore[visit4nd][1] as Int))
+			if (fr4nd[0] == visitScore[visit4nd][0]) {
+				fr4nd[1] = fr4nd[1] as Int + visitScore[visit4nd][1] as Int
 				visitScore.removeAt(visit4nd)
 			} else
 				visit4nd++
 		}
 	}
-
 	plusScore.addAll(visitScore)
 	result = organizeResult(plusScore)
 	return result
 }
 
-fun organizeResult(plusScore: MutableList<List<Any>>): List<String> {
+fun organizeResult(plusScore: MutableList<MutableList<Any>>): List<String> {
 	// 점수, 이름순 정렬
-	plusScore.sortWith(compareBy({ it[1] as Comparable<*> }, { it[0] as Comparable<*> }))
+	plusScore.sortWith(compareBy({ (it[1] as Int) * -1 }, { it[0] as String }))
 	// 5개까지만
 	val psLength = plusScore.size
 	if (psLength > 5) {
@@ -63,12 +60,20 @@ fun organizeResult(plusScore: MutableList<List<Any>>): List<String> {
 	return result
 }
 
-fun calcScore(friendsList: List<String>, i: Int): List<List<Any>> {
-// 4. 3에서 중복하는 만큼 +10  (ex : [a, 20], [j, 20])
-//	 4-1. 새로 들어오면, 생성 후 10
-//	 4-2. 이미 존재하면, 해당 아이디 +10
+fun calcScore(friendsList: List<String>, i: Int, userFriendsList: List<String>): MutableList<MutableList<Any>> {
 	val scoreList = mutableListOf<MutableList<Any>>()
 	for (friend in friendsList) {
+		// 내 친구 제외
+		var myFriend = false
+		for (user4nd in userFriendsList) {
+			if (friend == user4nd) {
+				myFriend = true
+				break
+			}
+		}
+		if (myFriend)
+			continue
+
 		var new = true
 		for (score in scoreList) {
 			if (friend == score[0]) {
@@ -80,24 +85,32 @@ fun calcScore(friendsList: List<String>, i: Int): List<List<Any>> {
 		if (new)
 			scoreList.add(mutableListOf(friend, i))
 	}
+
 	return scoreList
 }
 
-fun makeMutual4ndList(friendsList: List<String>, friends: List<List<String>>): List<String> {
-	val mutual4ndList = mutableListOf<String>()
-	for (userFriend in friendsList) {
-		mutual4ndList.addAll(make4ndsList(userFriend, friends))
+fun make4nd4ndList(userFriendsList: List<String>, friends: List<List<String>>, user: String): List<String> {
+	val friend4ndList = mutableListOf<String>()
+	for (userFriend in userFriendsList) {
+		friend4ndList.addAll(makeUser4ndsList(userFriend, friends))
 	}
-	return mutual4ndList
+	var numOfUser = 0
+	for (mf in friend4ndList)
+		if (mf == user)
+			numOfUser++
+	while (numOfUser-- > 0) {
+		friend4ndList.remove(user)
+	}
+	return friend4ndList
 }
 
-fun make4ndsList(user: String, friends: List<List<String>>): List<String> {
-	val friendsList = mutableListOf<String>()
+fun makeUser4ndsList(user: String, friends: List<List<String>>): List<String> {
+	val userFriendsList = mutableListOf<String>()
 	for (friend in friends) {
 		if (friend[0] == user)
-			friendsList.add(friend[1])
+			userFriendsList.add(friend[1])
 		else if (friend[1] == user)
-			friendsList.add(friend[0])
+			userFriendsList.add(friend[0])
 	}
-	return friendsList
+	return userFriendsList
 }
