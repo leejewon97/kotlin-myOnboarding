70ec472e53f5f54ed8ba60cb0ee611ed665f7481
Jtility <leejewon97@gmail.com>
2022-11-01 13:14:04 +0900

    make func list and modify 3<->6, rename one variable pr3

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,19 +1,21 @@
 package onboarding
 
+//1. curNum이 1부터 시작해서 number까지 수행되는 반복문
+//2. curNum의 한 자릿씩 잘라보면서 3,6,9 중 하나에 속하면 count++
+//3. return count
 fun solution3(number: Int): Int {
 //    TODO("프로그램 구현")
-    var count = 0
-    var carNum = 1
-    var digit = 0
-    while (carNum <= number) {
-        var tmp = carNum
-        while (tmp > 0) {
-            digit = tmp % 10
-            if (digit == 6 || digit == 3 || digit == 9)
-                count++
-            tmp /= 10
-        }
-        carNum++
-    }
-    return count
+	var count = 0
+	var curNum = 1
+	while (curNum <= number) {
+		var tmp = curNum
+		while (tmp > 0) {
+			var digit = tmp % 10
+			if (digit == 3 || digit == 6 || digit == 9)
+				count++
+			tmp /= 10
+		}
+		curNum++
+	}
+	return count
 }
